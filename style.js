f456f2ab46a564270fa2b58ddf5bedd0730cb127
Nasir zia <107996170+nasirzia133@users.noreply.github.com>
2023-04-13 14:38:36 +0000

    Add files via upload

---
+++ style.js
@@ -0,0 +1,27 @@
+// var Prime_number = prompt("Enter the prime Number");
+
+// if(Prime_number% 2!==0 || Prime_number==0 &&Prime_number%3!=0 || Prime_number == 3  &&Prime_number%5!=0 || Prime_number == 5 && Prime_number%7!=0 || Prime_number == 7){
+//     alert("prime Number")
+// }
+//    else{
+//     alert("number is not prime",Prime_number)
+//    }
+   
+
+   
+// var grade = prompt("Enter the Grade");
+
+// if(grade50){
+//     alert("pass")
+// }
+// else if(grade >50 && grade<70){
+//     alert("B")
+// }
+
+// else if(grade > 70 && grade <=80){
+
+//     alert("A")
+// }
+// else if (grade<50){
+//     alert("fail")
+// }
